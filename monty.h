18e6785fd0d637ffc5a0e422b14bd07163649398
badreaz <baz.23.2019@gmail.com>
2023-08-23 17:38:19 +0400

    add pall prototype

--- monty.h
+++ monty.h
@@ -54,6 +54,7 @@ typedef struct info_s
 extern info_t info;
 
 void (*get_opcode(void))(stack_t **stack, unsigned int line_number);
-void push(stack_t **stack, unsigned int line_number)
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
 
 #endif /*MAIN_H*/
