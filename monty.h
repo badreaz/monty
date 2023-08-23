9035e1b8495342d1869d0a178ca7929558726c3d
badreaz <baz.23.2019@gmail.com>
2023-08-23 17:54:32 +0400

    add pop prototype

--- monty.h
+++ monty.h
@@ -57,5 +57,6 @@ void (*get_opcode(void))(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 
 #endif /*MAIN_H*/
