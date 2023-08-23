11137705c14c61ac278b0457656b231250954c48
badreaz <baz.23.2019@gmail.com>
2023-08-23 18:27:17 +0400

    create nop.c

---
+++ nop.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+
+/**
+ * nop - does nothying.
+ * @stack: doubly linked list stack.
+ * @line_number: file line number.
+ */
+void nop(stack_t **stack, unsigned int line_number)__attribute__((unused))
+{
+}
