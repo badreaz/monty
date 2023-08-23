2ea91864ea4074b413c5b011c790a14cbb7fa4a2
badreaz <baz.23.2019@gmail.com>
2023-08-23 17:45:11 +0400

    create pint.c

---
+++ pint.c
@@ -0,0 +1,17 @@
+#include "mont.h"
+
+/**
+ * pint - print the value at the top of the stack.
+ * @stack: doubly linked stack.
+ * @line_number: file line number.
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (stack && *stack)
+		printf("%d\n", (*stack)->n);
+	else
+	{
+		fprintf(2, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
