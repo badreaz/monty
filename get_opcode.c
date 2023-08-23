cde872840042fc071d93f9ad0975cecdd58284c6
badreaz <baz.23.2019@gmail.com>
2023-08-23 17:16:49 +0400

    create get_opcode.c

---
+++ get_opcode.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * get_opcode - get the function for the opcode.
+ * 
+ * Return: poimter to the opcode function.
+ */
+void (*get_opcode(void))(stack_t **stack, unsigned int line_number)
+{
+	instruction_t ops[] = {
+		{"push", push};
+		{"pall", pall};
+		{NULL, NULL}};
+	int i;
+	char *opcode;
+
+	opcode = strtok(info.line, " \n");
+	info.value = strtok(NULL, " \n");
+	for (i = 0; ops[i].opcode; i++)
+	{
+		if (strcmp(opcode, ops[i].opcode) == 0)
+			return (ops[i].f);
+	}
+	return (NULL);
+}
