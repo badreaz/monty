58366372b4da82d3a58222d11e375f7a346201f3
badreaz <baz.23.2019@gmail.com>
2023-08-23 18:57:34 +0400

    skip unused parameter error

--- nop.c
+++ nop.c
@@ -5,6 +5,8 @@
  * @stack: doubly linked list stack.
  * @line_number: file line number.
  */
-void nop(stack_t **stack, unsigned int line_number)__attribute__((unused))
+void nop(stack_t **stack, unsigned int line_number)
 {
+	(void) stack;
+	(void) line_number;
 }
