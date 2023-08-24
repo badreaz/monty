33186e837ed91f75716aabe35459fe55c4f586fe
badreaz <baz.23.2019@gmail.com>
2023-08-24 21:09:35 +0400

    edit if statement condition

--- add.c
+++ add.c
@@ -7,7 +7,7 @@
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	if (!*stack || (*stack)->next)
+	if (!*stack || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		fclose(info.file);
