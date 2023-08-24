4e48509da7ce38b4290699ac3c52e8586b56f43b
badreaz <baz.23.2019@gmail.com>
2023-08-24 22:19:35 +0400

    create stack.c

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * stack - sets the format of data to stack.
+ * @stack: doubly linked list stack.
+ * @line_number: file line_number.
+ */
+void stack(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+
+	info.queue = 0;
+}
