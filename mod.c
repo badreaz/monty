acbe684fa33ac283cd70334a61d648666c1628e8
badreaz <baz.23.2019@gmail.com>
2023-08-24 21:07:46 +0400

    create mod.c

---
+++ mod.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * mod - computes the rest of division of the second and first
+ * elemnt of stack.
+ * @@stack: doubly linked list stack.
+ * @line_number: file line number.
+ */
+void mod(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		fclose(info.file);
+		free_stack(*stack);
+		free(info.line);
+		exit(EXIT_FAILURE);
+	}
+	else if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		fclose(info.file);
+		free_stack(*stack);	
+		free(info.line);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n %= (*stack)->n;
+	delete_dnodeint_at_index(stack, 0);
+}
