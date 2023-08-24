55df94b87b17b6c7f9df8356aec956cf53503de9
badreaz <baz.23.2019@gmail.com>
2023-08-25 01:50:46 +0400

    change error

--- monty.c
+++ monty.c
@@ -23,7 +23,7 @@ int main(int ac, char *av[])
 		fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE);
 	file = fopen(av[1], "r");
 	if (!file)
-		fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE);
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]), exit(EXIT_FAILURE);
 	line = malloc(sizeof(char) * size);
 	info.file = file;
 	while (ret != EOF)
