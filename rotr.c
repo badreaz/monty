bc773ab9dedc91bbb663c8d08f9b025475e22e9f
badreaz <baz.23.2019@gmail.com>
2023-08-24 21:47:49 +0400

    create rotr.c

---
+++ rotr.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * rotr - rotates the stack to the bottom.
+ * @stack: doubly linked list stack.
+ * @line_number: file line number.
+ */
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *h = *stack;
+	int i;
+
+	if (h && h->next)
+	{
+		for (i = 1; h->next; i++)
+			h = h->next;
+		add_dnodeint(stack, h->n);
+		delete_dnodeint_at_index(stack, i);
+	}
+}
