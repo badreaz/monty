2dd27c1b13f7de636d26977a841560dccaac25fa
badreaz <baz.23.2019@gmail.com>
2023-08-24 22:23:31 +0400

    create queue.c

---
+++ queue.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * queue - sets the format of data to queue.
+ * @stack: doubly linked list stack.
+ * @line_number: file line_number.
+ */
+void queue(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+
+	info.queue = 1;
+}
