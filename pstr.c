b920f6ed73a8e466cf36cf8484d879f874d05c7a
badreaz <baz.23.2019@gmail.com>
2023-08-24 21:31:55 +0400

    create pstr.c

---
+++ pstr.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * pstr - prints the string at the topp of stack.
+ * @stack: doubly linked list stack.
+ * @line_number:file line_number.
+ */
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *h = *stack;
+
+	while (h && h->n > 0 && h->n < 127)
+	{
+		printf("%c", h->n);
+		h = h->next;
+	}
+	printf("\n");
+}
